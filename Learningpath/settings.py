000d4717388edf8a0efdf4bf0e0729503d59bda6
Jide-codes <hezekiaholajide3@gmail.com>
2023-05-15 11:34:35 -1100

    Deploying to Render

--- Learningpath/settings.py
+++ Learningpath/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4cqyldr^!p9^y96rm8(qv=ot%tu##k^__&p-0c8m+p-v6ja3%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 SITE_ID =1  
 
