2200ab8dc4f8c406fa0f885471eed801270fc0fe
Jide-codes <hezekiaholajide3@gmail.com>
2023-05-17 08:53:39 -1100

    Debug

--- Learningpath/settings.py
+++ Learningpath/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4cqyldr^!p9^y96rm8(qv=ot%tu##k^__&p-0c8m+p-v6ja3%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
